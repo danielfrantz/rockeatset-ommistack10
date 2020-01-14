93a75a1e5b9b05598ed93b3a6f829ef56168e194
danielviannna <project.api99@gmail.com>
2020-01-14 20:21:13 -0300

    mongoose instalado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const mongoose = require('mogoose');
+const mongoose = require('mongoose');
 const app = express();
 
-// mongoose.connect('mongodb+srv://df-oministack:oministack@cluster0-1cojt.mongodb.net/weak10?retryWrites=true&w=majority', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
+mongoose.connect('mongodb+srv://df-oministack:oministack@cluster0-1cojt.mongodb.net/weak10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.get('/', (request, response) => {
     return response.json({message:'hello daneil'});
