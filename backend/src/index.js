d9e11fa49ede88a3f8c7a694859ccc51d28295b2
danielviannna <project.api99@gmail.com>
2020-01-14 19:57:07 -0300

    backend criado

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mogoose');
+const app = express();
+
+// mongoose.connect('mongodb+srv://df-oministack:oministack@cluster0-1cojt.mongodb.net/weak10?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+app.get('/', (request, response) => {
+    return response.json({message:'hello daneil'});
+});
+
+app.listen(3333);
